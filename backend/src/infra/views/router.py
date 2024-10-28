5bdfbcafbda9ef33f29da1c41efe5fa962abfd92
nnnLik <rslmakhmudov@gmail.com>
2024-10-29 02:58:29 +0300

    define ws entrypoint for events

--- backend/src/infra/views/router.py
+++ backend/src/infra/views/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from infra.views.auth_views import auth_router
+from infra.views.auth import auth_router
+from infra.views.ws_event_handler import ws_event_handler_router
 
 router = APIRouter(prefix='/api')
 
 router.include_router(auth_router, prefix='/auth')
+router.include_router(ws_event_handler_router, prefix='/events')
---
+++ backend/src/infra/views/ws_event_handler/ws_event_handler.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from fastapi.websockets import WebSocket
+
+from infra.repositories.connections import ConnectionRepository
+
+ws_event_handler_router = APIRouter()
+connection_repository = ConnectionRepository()
+
+
+@ws_event_handler_router.websocket('/event_handler/{user_id}')
+async def ws_entrypoint(ws: WebSocket, user_id: int) -> None:
+    await connection_repository.connect(
+        ws=WebSocket,
+        user_id=user_id,
+    )
